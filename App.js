c88f4cef45abc962f3f617fea2b7ac67bde07bd4
beeliu <belinda.liu@rocketmail.com>
2019-06-09 18:18:14 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import SearchField from './components/SearchField.js';
+import './App.css';
+
+class App extends Component{
+    render()
+    {
+      return(
+        <div>
+            <SearchField />
+        </div>
+      );
+    }
+}
+
+export default App;
